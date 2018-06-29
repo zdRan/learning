8b98f60b6b54d6f032d26a6fceaad49b92dc61e1
zdRan <cm.zdran@gmail.com>
2018-06-29 16:49:36 +0800

    整合log4j2

--- springboot/src/main/java/com/zdran/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/zdran/springboot/SpringbootApplication.java
@@ -3,10 +3,16 @@ package com.zdran.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动程序
+ * Create by zdRan on 2018/6/28
+ *
+ * @author cm.zdran@gmail.com
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
 }
