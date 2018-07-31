3db7cd0016f9cba8a4b799a076f5fdf7abd2a63d
zdRan <cm.zdran@gmail.com>
2018-07-31 16:13:54 +0800

    添加swagger依赖

--- springboot/src/main/java/com/zdran/springboot/dao/AccountInfo.java
+++ springboot/src/main/java/com/zdran/springboot/dao/AccountInfo.java
@@ -1,12 +1,40 @@
 package com.zdran.springboot.dao;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(
+        value = "AccountInfo",
+        description = "用户信息实体类"
+)
 public class AccountInfo {
+
+    @ApiModelProperty(
+            value = "账户id",
+            example = "123455",
+            dataType = "Integer"
+    )
     private Integer accountId;
 
+    @ApiModelProperty(
+            value = "用户名",
+            example = "zdran",
+            dataType = "String"
+    )
     private String name;
 
+    @ApiModelProperty(
+            value = "密码",
+            example = "zdran",
+            dataType = "String"
+    )
     private String pwd;
 
+    @ApiModelProperty(
+            value = "余额",
+            example = "zdran",
+            dataType = "Ingeger"
+    )
     private Integer balance;
 
     public Integer getAccountId() {
