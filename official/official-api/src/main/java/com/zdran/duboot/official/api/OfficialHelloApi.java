5d3b3202daea0b7047e746e3eca971ecaff94e76
zdRan <cm.zdran@gmail.com>
2019-01-28 11:20:03 +0800

    添加api

---
+++ official/official-api/src/main/java/com/zdran/duboot/official/api/OfficialHelloApi.java
@@ -0,0 +1,16 @@
+package com.zdran.duboot.official.api;
+
+/**
+ * Create by ranzd on 2019/1/28
+ *
+ * @author cm.zdran@gmail.com
+ */
+public interface OfficialHelloApi {
+    /**
+     * 返回带有 hello 的字符串
+     *
+     * @param name 名称
+     * @return
+     */
+    String sayHello(String name);
+}
