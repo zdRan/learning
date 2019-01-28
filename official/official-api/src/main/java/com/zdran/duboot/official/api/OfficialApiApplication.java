8d75e643d1caed84aa561a74df085bc7c9086d9a
zdRan <cm.zdran@gmail.com>
2019-01-28 11:16:00 +0800

    添加官方版的hello world

---
+++ official/official-api/src/main/java/com/zdran/duboot/official/api/OfficialApiApplication.java
@@ -0,0 +1,14 @@
+package com.zdran.duboot.official.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficialApiApplication.class, args);
+    }
+
+}
+
---
+++ official/official-user-provider/src/main/java/com/zdran/official/user/UserProviderApplication.java
@@ -0,0 +1,14 @@
+package com.zdran.official.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProviderApplication.class, args);
+	}
+
+}
+
