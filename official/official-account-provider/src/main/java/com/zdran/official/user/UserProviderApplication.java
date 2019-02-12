5c9f8a02444393f02d89eb20cddbcae0b8dccf32
zdRan <cm.zdran@gmail.com>
2019-02-12 10:58:34 +0800

    格式化代码

--- official/official-account-provider/src/main/java/com/zdran/official/user/UserProviderApplication.java
+++ official/official-account-provider/src/main/java/com/zdran/official/user/UserProviderApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserProviderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserProviderApplication.class, args);
+    }
 
 }
 
