1eef37e3f118aa21111bc5ab41c09392b852dc57
zdRan <cm.zdran@gmail.com>
2019-02-13 23:24:24 +0800

    学习Text组件的例子

---
+++ flutter/flutter_01/lib/TextDemo.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class TextDemo extends StatelessWidget{
+
+ 
+  
+  @override
+  Widget build(BuildContext context) {
+    var background = Paint();
+    background.color = Color.fromARGB(111, 255, 1, 1);
+    
+    var foreground = Paint();
+    foreground.color =  Color.fromARGB(255, 1, 122, 11);
+
+    var shadowsList = List<Shadow>();
+    shadowsList.add(Shadow(
+      color: Color.fromARGB(111, 255, 1, 1),
+      offset: Offset(1, 20),
+      blurRadius:1));
+    shadowsList.add(Shadow(
+      color: Color.fromARGB(255, 1, 122, 11),
+      offset: Offset(20, 1),
+      blurRadius:10));
+
+    return MaterialApp(
+      title: 'Text Demo',
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'hello ,text,好好学习,天天向上!好好学习,天天向上!好好学习,天天向上!好好学习,天天向上!好好学习,天天向上',
+            textAlign: TextAlign.start,
+            textDirection: TextDirection.rtl,
+            maxLines: 2,
+            
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              //background: background,
+              //color: Color.fromARGB(255, 1, 122, 11),
+              // decoration: TextDecoration.underline,
+              // decorationColor: Color.fromRGBO(255, 1, 1, 11),
+              // decorationStyle: TextDecorationStyle.wavy,
+              fontSize: 33,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w700,
+              //与color 值冲突
+              foreground: foreground,
+              height: 1.5,
+              letterSpacing: 11,
+              shadows: shadowsList,
+              wordSpacing: 11
+              ),
+              ),
+              
+        ),
+      ),
+    );
+  }
+
+} 
