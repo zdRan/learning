2740ff5fc6881b078d48568238aa3184765616d2
zdRan <cm.zdran@gmail.com>
2019-02-15 14:17:03 +0800

    更新代码

--- flutter/flutter_01/lib/TextDemo.dart
+++ flutter/flutter_01/lib/TextDemo.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(TextDemo());
 class TextDemo extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
     var background = Paint();
     background.color = Color.fromARGB(111, 255, 1, 1);
-    
     var foreground = Paint();
     foreground.color =  Color.fromARGB(255, 1, 122, 11);
 
@@ -29,23 +29,23 @@ class TextDemo extends StatelessWidget{
             textAlign: TextAlign.start,
             textDirection: TextDirection.rtl,
             maxLines: 2,
-            
-            overflow: TextOverflow.ellipsis,
+            overflow: TextOverflow.fade ,
+            //textScaleFactor: 1,
             style: TextStyle(
               //background: background,
               //color: Color.fromARGB(255, 1, 122, 11),
-              // decoration: TextDecoration.underline,
-              // decorationColor: Color.fromRGBO(255, 1, 1, 11),
-              // decorationStyle: TextDecorationStyle.wavy,
-              fontSize: 33,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w700,
+              decoration: TextDecoration.underline,
+              decorationColor: Color.fromRGBO(255, 1, 1, 11),
+              decorationStyle: TextDecorationStyle.wavy ,
+              fontSize: 44,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.w900,
               //与color 值冲突
               foreground: foreground,
               height: 1.5,
               letterSpacing: 11,
-              shadows: shadowsList,
-              wordSpacing: 11
+              //shadows: shadowsList,
+              wordSpacing: 122
               ),
               ),
               
