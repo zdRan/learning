016ead91876da962f07a3036968029f089540fa8
zdRan <cm.zdran@gmail.com>
2019-02-21 17:40:49 +0800

    重命名

---
+++ abstractFactory/src/JDMall.java
@@ -0,0 +1,34 @@
+import model.Goods;
+import model.Order;
+import util.HttpClientUtil;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 具体的工厂类，封装京东API
+ * Create by ranzd on 2019/2/21
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class JDMall implements Mall {
+    @Override
+    public Goods getGoodsInfo(String sku) {
+        String result = HttpClientUtil.getMethod("jd/url/getGoodsInfo", new HashMap<>());
+        return this.getGoodsByResult(result);
+    }
+
+    @Override
+    public Order submitOrder(List<Goods> goods) {
+        String result = HttpClientUtil.getMethod("jd/url/submitOrder", new HashMap<>());
+        return this.getOrderByResult(result);
+    }
+
+    private Goods getGoodsByResult(String result) {
+        return new Goods();
+    }
+
+    private Order getOrderByResult(String result) {
+        return new Order();
+    }
+}
---
+++ abstractFactory/src/Mall.java
@@ -0,0 +1,30 @@
+import model.Goods;
+import model.Order;
+
+import java.util.List;
+
+/**
+ * 工厂类
+ * Create by ranzd on 2019/2/20
+ *
+ * @author cm.zdran@gmail.com
+ */
+public interface Mall {
+    /**
+     * 工厂方法：获取商品信息
+     *
+     * @param sku 商品 id
+     * @return 商品信息
+     */
+    Goods getGoodsInfo(String sku);
+
+    /**
+     * 工厂方法：下单接口
+     *
+     * @param goods 商品集合
+     * @return 订单
+     */
+    Order submitOrder(List<Goods> goods);
+
+
+}
