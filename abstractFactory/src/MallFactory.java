7962b6f6a712f0e08e65704688fe5317f2ffe574
zdRan <cm.zdran@gmail.com>
2019-02-21 17:31:32 +0800

    添加工厂方法

--- abstractFactory/src/MallFactory.java
+++ abstractFactory/src/MallFactory.java
@@ -1,9 +1,30 @@
+import model.Goods;
+import model.Order;
+
+import java.util.List;
+
 /**
- * 抽象工厂类，产生具体的工厂
+ * 工厂类
  * Create by ranzd on 2019/2/20
  *
  * @author cm.zdran@gmail.com
  */
 public interface MallFactory {
+    /**
+     * 工厂方法：获取商品信息
+     *
+     * @param sku 商品 id
+     * @return 商品信息
+     */
+    Goods getGoodsInfo(String sku);
+
+    /**
+     * 工厂方法：下单接口
+     *
+     * @param goods 商品集合
+     * @return 订单
+     */
+    Order submitOrder(List<Goods> goods);
+
 
 }
