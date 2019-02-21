7b53d8021f241390084b9995a7255b95591894da
zdRan <cm.zdran@gmail.com>
2019-02-21 18:00:09 +0800

    添加使用入口

---
+++ abstractFactory/src/MallController.java
@@ -0,0 +1,30 @@
+import model.Goods;
+import model.Order;
+
+import java.util.List;
+
+/**
+ * 平台模块入口
+ * Create by ranzd on 2019/2/21
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class MallController {
+
+    public Goods getGoodsInfo(String sku, String channel) {
+        Mall mall = MallFactory.getMall(channel);
+        if (mall == null) {
+            return null;
+        }
+        return mall.getGoodsInfo(sku);
+    }
+
+    public Order submitOrder(List<Goods> goods, String channel) {
+        Mall mall = MallFactory.getMall(channel);
+        if (mall == null) {
+            return null;
+        }
+        return mall.submitOrder(goods);
+    }
+
+}
