3be00f9db1eabefc00c988f9e52cc551b37913cd
zdRan <cm.zdran@gmail.com>
2019-02-21 17:31:22 +0800

    添加Http 客户端

---
+++ abstractFactory/src/util/HttpClientUtil.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.Map;
+
+/**
+ * Http 客户端
+ * Create by ranzd on 2019/2/21
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class HttpClientUtil {
+
+    public static String getMethod(String url, Map<String, String> param) {
+        return "";
+    }
+    public static String postMethod(String url, Map<String, String> param) {
+        return "";
+    }
+}
