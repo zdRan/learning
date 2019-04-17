04bf86c8bda8d9b5f2dc4157105f22b7c914366f
zdRan <cm.zdran@gmail.com>
2019-04-17 18:07:22 +0800

    格式化导入包

--- springboot/src/main/java/com/zdran/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/zdran/springboot/SpringbootApplication.java
@@ -3,8 +3,6 @@ package com.zdran.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
